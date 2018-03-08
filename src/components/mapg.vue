3234e2d67eee71e233e91cd24ef93171ba2053fe
tristusdelatristussie <tristus@hotmail.com>
2018-03-08 16:26:03 +0100

    mapg template update

--- src/components/mapg.vue
+++ src/components/mapg.vue
@@ -1,3 +1,74 @@
+<template>
+<div id="mapg">
+        <div class="row">
+                    <div class="input-field col s6">
+<gmap-map class="map-container" :center="center" :zoom="12">
+    <gmap-marker
+      :position="position"
+      :clickable="true"
+      :draggable="true"
+      @click="center=position">
+    </gmap-marker>
+    <gmap-info-window :position="{lat:48.8545797, lng:2.3342732}">
+      Hello world!
+    </gmap-info-window>
+  </gmap-map>
+
+  
+  </div>
+                 <div class="input-field col s6">
+
+          <div class="input-field col s6">
+              <p>Code postal :</p>
+            <input id="postal" type="number" class="validate">
+        </div>
+        <div class="input-field col s6">
+            <p>Adresse :</p>
+          <input placeholder="adresse" id="adresse" type="text" class="validate">
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s6 ">
+            <p>Nom du magasin : </p>
+          <input placeholder="Nom du magasin" id="magasin" type="text" class="validate" v-model="mag">
+        </div>
+        <div>{{res}}</div>
+        <button type="submit" @click="f">Envoyez</button>
+      </div>
+      </div>
+
+
+</div>
+</template>
+
+<script>
+import * as VueGoogleMaps from "vue2-google-maps";
+
+
+const geolocation = require("google-geolocation")({
+  key: "AIzaSyAM3jmpomtVXxPskyOSDZ9k7rr_CEo_Jvc",
+  timeout: 2000
+});
+
+
+
+
+
+const params = {
+  wifiAccessPoints: [
+    {
+      macAddress: "01:23:45:67:89:AB",
+      signalStrength: -65,
+      signalToNoiseRatio: 40
+    }
+  ]
+};
+
+var googleMapsClient = require("@google/maps").createClient({
+  key: "AIzaSyAM3jmpomtVXxPskyOSDZ9k7rr_CEo_Jvc"
+});
+
+
 export default {
   data () {
     return {
@@ -48,3 +119,12 @@ export default {
     this.g()
   }
 }
+</script>
+
+<style>
+.map-container {
+  width: 45vw;
+  height: 50vh;
+}
+</style>
+
